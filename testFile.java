2bea0f4f7b0b54067f6b14c0ee7f4e59616246d1
dennisthomas1995 <50439803+dennisthomas1995@users.noreply.github.com>
2019-05-09 12:41:55 +0530

    Create testFile.java

---
+++ testFile.java
@@ -0,0 +1,20 @@
+public class test {
+  String name;
+  int age;
+  
+  private void setName(String name){
+    this.name = name;
+  }
+  
+  private String getName(){
+    return name;
+  }
+  
+  private void setAge(int age){
+    this.age = age;
+  }
+  
+  private String getAge(){
+    return age;
+  }
+}
